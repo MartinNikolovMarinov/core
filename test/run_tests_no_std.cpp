c15af2b78a5016d083f732f9dfbbbf262843f542
MartinNikolovMarinov <martin_n_marinov@protonmail.com>
2022-09-02 18:55:43 +0300

    slight change to tests

--- test/run_tests_no_std.cpp
+++
@@ -1,9 +0,0 @@
-#include "t-bits.cpp"
-#include "t-rnd.cpp"
-#include "t-rune.cpp"
-
-void RunAllTests() {
-    RunTestSuite(RunBitsTestsSuite);
-    RunTestSuite(RunRndTestsSuite);
-    RunTestSuite(RunRuneTestsSuite);
-}

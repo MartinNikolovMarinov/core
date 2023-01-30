d47265293b761105de2a81ba050af185af6f0a3d
Martin Marinov <martin_n_marinov@protonmail.com>
2023-01-30 20:19:39 +0200

    slight gloabal state refactor

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,89 +3,112 @@
 #include <iostream>
 #include <chrono>
 
-// GLOBAL STATE:
-static keyboard g_keyboard;
-static mouse g_mouse;
-static bool g_render = true;
-static bool g_keyboardStateChange = true;
-static bool g_mouseStateChange = true;
-static bool g_windowStateChange = true;
-static std::chrono::time_point<std::chrono::high_resolution_clock> g_lastWindowDragTime;
+enum app_exit_codes : i32 {
+    APP_EXIT_SUCCESS = 0,
+    APP_EXIT_FAILED_TO_INIT = -1,
+};
 
-i32 main() {
+struct AppState {
+    keyboard keyboardState;
+    mouse mouseState;
+    bool keyboardStateChange;
+    bool mouseStateChange;
+    bool windowStateChange;
+    std::chrono::time_point<std::chrono::high_resolution_clock> lastWindowDragTime;
+    core::vec4<f32> clearColor;
+    GLFWwindow* glfwWindow;
+    i32 windowWidth;
+    i32 windowHeight;
+    const char* windowTitle;
+};
+static AppState g_appState;
+
+GLFWwindow* init_glfw_window(i32 width, i32 height, const char* title) {
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
+        return nullptr;
     }
-    defer { glfwTerminate(); };
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+
+    // Hints for OpenGL:
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Example Window", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(width, height, title, nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
-        return -1;
+        return nullptr;
     }
     glfwMakeContextCurrent(window);
 
-    if (auto err = glewInit(); err != GLEW_OK) {
-        std::cerr << "Failed to initialize GLEW reason: " << glewGetErrorString(err) << std::endl;
-        return -1;
-    }
-
     glfwSetInputMode(window, GLFW_LOCK_KEY_MODS, GLFW_TRUE);
+
+    // Store some of the window state in the global state:
+    g_appState.glfwWindow = window;
+    g_appState.windowWidth = width;
+    g_appState.windowHeight = height;
+    g_appState.windowTitle = title;
+
+    return window;
+}
+
+void init_glfw_event_handlers(GLFWwindow* window) {
+    // TODO: All these callbacks can return nullptr in case of error. Not checking anything is not great.
+
+    // Error handler
 
     glfwSetErrorCallback([](i32, const char* description) {
         std::cerr << "GLFW Error: " << description << std::endl;
     });
 
-    // Keyboard callbacks
+    // Keyboard event handlers
 
     glfwSetKeyCallback(window, [](GLFWwindow* window, i32 key, i32 scancode, i32 action, i32 mods) {
         keyboard_modifiers keyModifiers = keyboard_modifiers::create_from_glfw(mods);
         key_info keyInfo = key_info::create_from_glfw(key, scancode, action);
-        g_keyboard.set_modifiers(core::move(keyModifiers))
+        g_appState.keyboardState.set_modifiers(core::move(keyModifiers))
                   .set_key(core::move(keyInfo));
 
         if (keyInfo.value == GLFW_KEY_ESCAPE && keyInfo.is_pressed()) {
             glfwSetWindowShouldClose(window, GLFW_TRUE);
         }
 
-        g_keyboardStateChange = true;
+        g_appState.keyboardStateChange = true;
     });
 
     glfwSetCharCallback(window, [](GLFWwindow*, u32 codepoint) {
-        g_keyboard.set_text_input(core::rune(codepoint));
-        g_keyboardStateChange = true;
+        g_appState.keyboardState.set_text_input(core::rune(codepoint));
+        g_appState.keyboardStateChange = true;
     });
 
-    // Mouse callbacks
+    // Mouse event handlers
 
     glfwSetCursorPosCallback(window, [](GLFWwindow*, f64 xpos, f64 ypos) {
-        g_mouse.set_pos(xpos, ypos);
-        g_mouseStateChange = true;
+        g_appState.mouseState.set_pos(xpos, ypos);
+        g_appState.mouseStateChange = true;
     });
 
     glfwSetCursorEnterCallback(window, [](GLFWwindow*, i32 entered) {
-        g_mouse.set_in_window(entered != 0);
-        g_mouseStateChange = true;
+        g_appState.mouseState.set_in_window(entered != 0);
+        g_appState.mouseStateChange = true;
     });
 
     glfwSetMouseButtonCallback(window, [](GLFWwindow*, i32 button, i32 action, i32 mods) {
         // Change keyboard state to take account of the modifiers:
         keyboard_modifiers keyModifiers = keyboard_modifiers::create_from_glfw(mods);
-        g_keyboard.set_modifiers(core::move(keyModifiers));
-        g_keyboardStateChange = true;
+        g_appState.keyboardState.set_modifiers(core::move(keyModifiers));
+        g_appState.keyboardStateChange = true;
         // Then change the mouse state:
         key_info mouseButton = key_info::create_from_glfw(button, 0, action);
-        g_mouse.set_button(mouseButton);
-        g_mouseStateChange = true;
+        g_appState.mouseState.set_button(mouseButton);
+        g_appState.mouseStateChange = true;
     });
 
     glfwSetScrollCallback(window, [](GLFWwindow*, f64 xoffset, f64 yoffset) {
-        g_mouse.set_scroll(xoffset, yoffset);
-        g_mouseStateChange = true;
+        g_appState.mouseState.set_scroll(xoffset, yoffset);
+        g_appState.mouseStateChange = true;
     });
 
     glfwSetDropCallback(window, [](GLFWwindow*, i32 count, const char** paths) {
@@ -95,77 +118,120 @@ i32 main() {
         for (i32 i = 0; i < count; ++i) {
             std::cout << paths[i] << std::endl;
         }
-        g_mouseStateChange = true;
+        g_appState.mouseStateChange = true;
     });
 
-    // Window callbacks
+    // Window event handlers
 
     glfwSetFramebufferSizeCallback(window, [](GLFWwindow*, i32 width, i32 height) {
         std::cout << "Window Resize to: " << width << ", " << height << std::endl;
-        g_windowStateChange = true;
+        g_appState.windowWidth = width;
+        g_appState.windowHeight = height;
+        glViewport(0, 0, width, height);
+        g_appState.windowStateChange = true;
     });
 
     glfwSetWindowPosCallback(window, [](GLFWwindow*, i32 xpos, i32 ypos) {
         auto now = std::chrono::high_resolution_clock::now();
-        auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(now - g_lastWindowDragTime);
+        auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(now - g_appState.lastWindowDragTime);
         if (diff.count() > 50) {
             // Throttle window drag events to avoid spam rendering.
             std::cout << "Window dragged to: " << xpos << ", " << ypos << std::endl;
-            g_lastWindowDragTime = std::chrono::high_resolution_clock::now();
-            g_windowStateChange = true;
+            g_appState.lastWindowDragTime = std::chrono::high_resolution_clock::now();
+            g_appState.windowStateChange = true;
         }
     });
 
     glfwSetWindowCloseCallback(window, [](GLFWwindow* window) {
         std::cout << "Window close requested" << std::endl;
         glfwSetWindowShouldClose(window, true);
-        g_windowStateChange = true;
+        g_appState.windowStateChange = true;
     });
 
     glfwSetWindowFocusCallback(window, [](GLFWwindow*, i32 focused) {
         std::cout << "Window " << (focused == 0 ? "unfocused" : "focused") << std::endl;
-        g_windowStateChange = true;
+        g_appState.windowStateChange = true;
     });
 
     glfwSetWindowMaximizeCallback(window, [](GLFWwindow*, i32 maximized) {
         std::cout << "Window " << (maximized == 0 ? "unmaximized" : "maximized") << std::endl;
-        g_windowStateChange = true;
+        g_appState.windowStateChange = true;
     });
 
     glfwSetWindowContentScaleCallback(window, [](GLFWwindow*, f32 xscale, f32 yscale) {
         std::cout << "Window content scale: " << xscale << ", " << yscale << std::endl;
-        g_windowStateChange = true;
+        g_appState.windowStateChange = true;
     });
 
     glfwSetWindowRefreshCallback(window, [](GLFWwindow*) {
         std::cout << "Window refresh requested" << std::endl;
-        g_windowStateChange = true;
+        g_appState.windowStateChange = true;
     });
+}
+
+void init_openGL() {
+    glViewport(0, 0, g_appState.windowWidth, g_appState.windowHeight);
+}
+
+bool should_render() {
+    bool shouldRender = g_appState.windowStateChange ||
+                        g_appState.mouseStateChange ||
+                        g_appState.keyboardStateChange;
+    return shouldRender;
+}
+
+void prepare_state_for_next_frame() {
+    g_appState.keyboardStateChange = false;
+    g_appState.mouseStateChange = false;
+    g_appState.windowStateChange = false;
+    g_appState.keyboardState.clear();
+    g_appState.mouseState.clear();
+
+    // Clear the screen:
+    auto& cc = g_appState.clearColor;
+    glClearColor(cc.r(), cc.g(), cc.b(), cc.a());
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+i32 main() {
+    g_appState = {}; // Zero out the global state.
+    g_appState.windowStateChange = true; // Force a render on the first frame.
+    g_appState.windowWidth = 800;
+    g_appState.windowHeight = 600;
+    g_appState.windowTitle = "Hello World";
+    g_appState.clearColor = core::v(0.2f, 0.3f, 0.3f, 1.0f);
+
+    GLFWwindow* window = init_glfw_window(g_appState.windowWidth, g_appState.windowHeight, g_appState.windowTitle);
+    defer { glfwTerminate(); };
+    if (!window) return APP_EXIT_FAILED_TO_INIT;
+    defer { glfwDestroyWindow(window); };
+
+    init_glfw_event_handlers(window);
+
+    // Init glew after we have a window:
+    if (auto err = glewInit(); err != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW reason: " << glewGetErrorString(err) << std::endl;
+        return APP_EXIT_FAILED_TO_INIT;
+    }
 
     while(!glfwWindowShouldClose(window)) {
-        // Swap buffers:
-        g_render = g_windowStateChange || g_mouseStateChange || g_keyboardStateChange;
-        if (g_render) glfwSwapBuffers(window);
+        if (should_render()) {
+            glfwSwapBuffers(window);
+        }
 
-        // Clear state:
-        g_render = false;
-        g_keyboardStateChange = false;
-        g_mouseStateChange = false;
-        g_windowStateChange = false;
-        g_keyboard.clear();
-        g_mouse.clear();
+        prepare_state_for_next_frame();
 
         // Poll/Wait events:
         // glfwPollEvents();
         glfwWaitEvents();
 
-        if (g_keyboardStateChange) {
-            std::cout << g_keyboard.to_string();
+        if (g_appState.keyboardStateChange) {
+            std::cout << g_appState.keyboardState.to_string();
         }
-        if (g_mouseStateChange) {
-            std::cout << g_mouse.to_string();
+        if (g_appState.mouseStateChange) {
+            std::cout << g_appState.mouseState.to_string();
         }
     }
 
-    return 0;
+    return APP_EXIT_SUCCESS;
 }

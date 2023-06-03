cbfd7e37b2955f4e84483f4f8f3fe91f069517cb
Martin Marinov <martin_n_marinov@protonmail.com>
2023-06-03 23:36:18 +0300

    slight tuple refactor

--- src/tuple.h
+++ src/tuple.h
@@ -3,9 +3,7 @@
 #include <API.h>
 #include <types.h>
 #include <utils.h>
-
-// FIXME: Make this thing more usable. Every time I try to use it I spend inordinate amount of time trying to figure out
-//        how to use it. It should have a much more convinient and obvious constructor. The get function is kinda fine.
+#include <core_traits.h>
 
 namespace core {
 
@@ -13,40 +11,10 @@ using namespace coretypes;
 
 template <typename...> struct tuple;
 
-template <typename T1>
-struct CORE_API_EXPORT tuple<T1> {
-    static constexpr u32 len = 1;
-
-    constexpr tuple() : v1({}) {}
-    constexpr tuple(T1&& a) : v1(a) {}
-
-    template <i32 TIdx>
-    constexpr auto& get() {
-        if constexpr (TIdx == 0) return v1;
-    }
-    template <i32 TIdx>
-    constexpr const auto& get() const {
-        // NOTE:
-        // Can't reuse the non-const version because it uses auto + constexpr and can't deduce the type.
-        // Or it takes too much efford to research how to do this and I am lazy.
-        // Either way, copy paste like an idiot. Thanks C++.
-        if constexpr (TIdx == 0) return v1;
-    }
-
-    // friends
-    template <typename...T> friend constexpr tuple<T...> create_tuple(T&&... args);
-
-private:
-    T1 v1;
-};
-
 template <typename T1, typename T2>
 struct CORE_API_EXPORT tuple<T1, T2> {
     static constexpr u32 len = 2;
 
-    constexpr tuple() : v1({}), v2({}) {}
-    constexpr tuple(T1&& a, T2&& b) : v1(a), v2(b) {}
-
     template <i32 TIdx>
     constexpr auto& get() {
         if      constexpr (TIdx == 0) return v1;
@@ -58,10 +26,6 @@ struct CORE_API_EXPORT tuple<T1, T2> {
         else if constexpr (TIdx == 1) return v2;
     }
 
-    // friends
-    template <typename...T> friend constexpr tuple<T...> create_tuple(T&&... args);
-
-private:
     T1 v1;
     T2 v2;
 };
@@ -70,9 +34,6 @@ template <typename T1, typename T2, typename T3>
 struct CORE_API_EXPORT tuple<T1, T2, T3> {
     static constexpr u32 len = 3;
 
-    constexpr tuple() : v1({}), v2({}), v3({}) {}
-    constexpr tuple(T1&& a, T2&& b, T3&& c) : v1(a), v2(b), v3(c) {}
-
     template <i32 TIdx>
     constexpr auto& get() {
         if      constexpr (TIdx == 0) return v1;
@@ -86,10 +47,6 @@ struct CORE_API_EXPORT tuple<T1, T2, T3> {
         else if constexpr (TIdx == 2) return v3;
     }
 
-    // friends
-    template <typename...T> friend constexpr tuple<T...> create_tuple(T&&... args);
-
-private:
     T1 v1;
     T2 v2;
     T3 v3;
@@ -99,9 +56,6 @@ template <typename T1, typename T2, typename T3, typename T4>
 struct CORE_API_EXPORT tuple<T1, T2, T3, T4> {
     static constexpr u32 len = 4;
 
-    constexpr tuple() : v1({}), v2({}), v3({}), v4({}) {}
-    constexpr tuple(T1&& a, T2&& b, T3&& c, T4&& d) : v1(a), v2(b), v3(c), v4(d) {}
-
     template <i32 TIdx>
     constexpr auto& get() {
         if      constexpr (TIdx == 0) return v1;
@@ -117,10 +71,6 @@ struct CORE_API_EXPORT tuple<T1, T2, T3, T4> {
         else if constexpr (TIdx == 3) return v4;
     }
 
-    // friends
-    template <typename...T> friend constexpr tuple<T...> create_tuple(T&&... args);
-
-private:
     T1 v1;
     T2 v2;
     T3 v3;
@@ -129,7 +79,25 @@ private:
 
 template <typename...TArgs>
 constexpr CORE_API_EXPORT tuple<TArgs...> create_tuple(TArgs&&... args) {
-    return tuple<TArgs...>(core::forward<TArgs>(args)...);
+    constexpr i32 NArgs = sizeof...(TArgs);
+    static_assert(1 < NArgs && NArgs <= 4, "tuples can only have 2, 3 or 4 elements");
+    if constexpr (NArgs == 2) {
+        return tuple<TArgs...>{ core::forward<TArgs>(args)... };
+    }
+    else if constexpr (NArgs == 3) {
+        return tuple<TArgs...>{ core::forward<TArgs>(args)... };
+    }
+    else {
+        return tuple<TArgs...>{ core::forward<TArgs>(args)... };
+    }
 }
+
+template <typename T> using pair   = tuple<T, T>;
+template <typename T> using triple = tuple<T, T, T>;
+template <typename T> using quad   = tuple<T, T, T, T>;
+
+static_assert(core::IsTrivial_v<tuple<i32, i32>>);
+static_assert(core::IsTrivial_v<tuple<i32, i32, i32>>);
+static_assert(core::IsTrivial_v<tuple<i32, i32, i32, i32>>);
 
 } // namespace core
--- test/t-tuple.cpp
+++ test/t-tuple.cpp
@@ -20,6 +20,9 @@ void run_tuple_argument_increment() {
     auto& ref = t1.get<2>();
     ref++;
     Assert(t1.get<2>() == 7);
+    auto& [a, b, c] = t1;
+    c++;
+    Assert(t1.get<2>() == 8);
 
     Assert(t1.get<0>() == t2.get<0>());
     Assert(t1.get<1>() == t2.get<1>());
@@ -27,25 +30,39 @@ void run_tuple_argument_increment() {
 }
 
 void run_create_tuples_of_different_sizes() {
-    auto t1 = core::create_tuple(1);
-    auto t2 = core::create_tuple(1, 2);
-    auto t3 = core::create_tuple(1, 2, 3);
-    auto t4 = core::create_tuple(1, 2, 3, 4);
-
-    Assert(t1.len == 1);
-    Assert(t1.get<0>() == 1);
-    Assert(t2.len == 2);
-    Assert(t2.get<0>() == 1);
-    Assert(t2.get<1>() == 2);
-    Assert(t3.len == 3);
-    Assert(t3.get<0>() == 1);
-    Assert(t3.get<1>() == 2);
-    Assert(t3.get<2>() == 3);
-    Assert(t4.len == 4);
-    Assert(t4.get<0>() == 1);
-    Assert(t4.get<1>() == 2);
-    Assert(t4.get<2>() == 3);
-    Assert(t4.get<3>() == 4);
+    {
+        auto t = core::create_tuple(1, 2);
+        Assert(t.len == 2);
+        Assert(t.get<0>() == 1);
+        Assert(t.get<1>() == 2);
+        auto [first, second] = t;
+        Assert(first == 1);
+        Assert(second == 2);
+    }
+    {
+        auto t = core::create_tuple(1, 2, 3);
+        Assert(t.len == 3);
+        Assert(t.get<0>() == 1);
+        Assert(t.get<1>() == 2);
+        Assert(t.get<2>() == 3);
+        auto [first, second, third] = t;
+        Assert(first == 1);
+        Assert(second == 2);
+        Assert(third == 3);
+    }
+    {
+        auto t = core::create_tuple(1, 2, 3, 4);
+        Assert(t.len == 4);
+        Assert(t.get<0>() == 1);
+        Assert(t.get<1>() == 2);
+        Assert(t.get<2>() == 3);
+        Assert(t.get<3>() == 4);
+        auto [first, second, third, fourth] = t;
+        Assert(first == 1);
+        Assert(second == 2);
+        Assert(third == 3);
+        Assert(fourth == 4);
+    }
 }
 
 void run_tuple_tests_suite() {

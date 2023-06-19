eb1a22cf4b109d525babfdd9fa1c3b5e4822e0d4
Martin Marinov <martin_n_marinov@protonmail.com>
2023-06-19 18:22:55 +0300

    arr.h can't be a constexpr because it has reinterpret casting

--- src/arr.h
+++ src/arr.h
@@ -20,15 +20,15 @@ struct CORE_API_EXPORT arr {
     static constexpr bool dataIsTrivial = core::IsTrivial_v<data_type>;
     static constexpr bool dataHasTrivialDestructor = core::IsTriviallyDestructible_v<data_type>;
 
-    constexpr arr() : m_data(nullptr), m_cap(0), m_len(0) {}
-    constexpr arr(size_type len) : m_data(nullptr), m_cap(len), m_len(len) {
+    arr() : m_data(nullptr), m_cap(0), m_len(0) {}
+    arr(size_type len) : m_data(nullptr), m_cap(len), m_len(len) {
         if (m_cap > 0) {
             m_data = reinterpret_cast<data_type *>(core::alloc<allocator_type>(m_cap * sizeof(data_type)));
             Assert(m_data != nullptr);
             callDefaultCtorsIfTypeIsNonTrivial();
         }
     }
-    constexpr arr(size_type len, size_type cap) : m_data(nullptr), m_cap(cap), m_len(len) {
+    arr(size_type len, size_type cap) : m_data(nullptr), m_cap(cap), m_len(len) {
         Assert(m_cap >= m_len);
         if (m_cap > 0) {
             m_data = reinterpret_cast<data_type *>(core::alloc<allocator_type>(m_cap * sizeof(data_type)));
@@ -36,14 +36,14 @@ struct CORE_API_EXPORT arr {
             callDefaultCtorsIfTypeIsNonTrivial();
         }
     }
-    constexpr arr(const arr& other) : m_data(nullptr), m_cap(other.m_cap), m_len(other.m_len) {
+    arr(const arr& other) : m_data(nullptr), m_cap(other.m_cap), m_len(other.m_len) {
         if (m_cap > 0) {
             m_data = reinterpret_cast<data_type *>(core::alloc<allocator_type>(m_cap * sizeof(data_type)));
             Assert(m_data != nullptr);
             copyDataAt(other.m_data, 0, m_len);
         }
     }
-    constexpr arr(arr&& other) : m_data(other.m_data), m_cap(other.m_cap), m_len(other.m_len) {
+    arr(arr&& other) : m_data(other.m_data), m_cap(other.m_cap), m_len(other.m_len) {
         if (this == &other) return;
         other.m_data = nullptr;
         other.m_cap = 0;
@@ -53,8 +53,8 @@ struct CORE_API_EXPORT arr {
 
     // Prevent copy assignment, because it is unwanted behaviour most of the time.
     //This does not prevent auto arr2 = arr1; but prevents arr2 = arr1; !
-    constexpr arr& operator=(const arr& other) = delete;
-    constexpr arr& operator=(arr&& other) {
+    arr& operator=(const arr& other) = delete;
+    arr& operator=(arr&& other) {
         if (this == &other) return *this;
         free(); // very important to free before assigning new data.
         m_data = other.m_data;
@@ -66,20 +66,20 @@ struct CORE_API_EXPORT arr {
         return *this;
     }
 
-    constexpr const char* allocator_name() const { return core::allocator_name<allocator_type>(); }
-    constexpr size_type   cap()            const { return m_cap; }
-    constexpr size_type   len()            const { return m_len; }
-    constexpr size_type   byte_len()       const { return m_len * sizeof(data_type); }
-    constexpr data_type*  data()           const { return m_data; }
-    constexpr bool        empty()          const { return m_len == 0; }
-    constexpr void        clear()                { m_len = 0; }
+    const char* allocator_name() const { return core::allocator_name<allocator_type>(); }
+    size_type   cap()            const { return m_cap; }
+    size_type   len()            const { return m_len; }
+    size_type   byte_len()       const { return m_len * sizeof(data_type); }
+    data_type*  data()           const { return m_data; }
+    bool        empty()          const { return m_len == 0; }
+    void        clear()                { m_len = 0; }
 
-    constexpr arr<T, TAllocator> copy() {
+    arr<T, TAllocator> copy() {
         arr<T, TAllocator> result(*this);
         return result;
     }
 
-    constexpr void free() {
+    void free() {
         if (m_data == nullptr) return;
         if constexpr (!dataHasTrivialDestructor) {
             // For elements that are not trivially destructible call destructors manually:
@@ -93,17 +93,17 @@ struct CORE_API_EXPORT arr {
         m_data = nullptr;
     }
 
-    constexpr data_type& at(size_type idx)                     { Assert(idx <= m_len); return m_data[idx]; }
-    constexpr const data_type& at(size_type idx) const         { Assert(idx <= m_len); return m_data[idx]; }
-    constexpr data_type& operator[](size_type idx)             { return at(idx); }
-    constexpr const data_type& operator[](size_type idx) const { return at(idx); }
+    data_type& at(size_type idx)                     { Assert(idx <= m_len); return m_data[idx]; }
+    const data_type& at(size_type idx) const         { Assert(idx <= m_len); return m_data[idx]; }
+    data_type& operator[](size_type idx)             { return at(idx); }
+    const data_type& operator[](size_type idx) const { return at(idx); }
 
-    constexpr data_type& first()             { Assert(m_len > 0); return m_data[0]; }
-    constexpr const data_type& first() const { Assert(m_len > 0); return m_data[0]; }
-    constexpr data_type& last()              { Assert(m_len > 0); return m_data[m_len - 1]; }
-    constexpr const data_type& last() const  { Assert(m_len > 0); return m_data[m_len - 1]; }
+    data_type& first()             { Assert(m_len > 0); return m_data[0]; }
+    const data_type& first() const { Assert(m_len > 0); return m_data[0]; }
+    data_type& last()              { Assert(m_len > 0); return m_data[m_len - 1]; }
+    const data_type& last() const  { Assert(m_len > 0); return m_data[m_len - 1]; }
 
-    constexpr arr& append(const data_type& val) {
+    arr& append(const data_type& val) {
         if (m_len == m_cap) {
             reserve(m_cap == 0 ? 1 : m_cap * 2);
         }
@@ -112,7 +112,7 @@ struct CORE_API_EXPORT arr {
         return *this;
     }
 
-    constexpr arr& append(data_type&& val) {
+    arr& append(data_type&& val) {
         if (m_len == m_cap) {
             reserve(m_cap == 0 ? 1 : m_cap * 2);
         }
@@ -121,7 +121,7 @@ struct CORE_API_EXPORT arr {
         return *this;
     }
 
-    constexpr arr& append(const data_type* val, size_type len) {
+    arr& append(const data_type* val, size_type len) {
         if (m_len + len > m_cap) {
             reserve(m_cap == 0 ? len : m_cap * 2);
         }
@@ -130,14 +130,14 @@ struct CORE_API_EXPORT arr {
         return *this;
     }
 
-    constexpr arr& fill(const data_type& val) {
+    arr& fill(const data_type& val) {
         for (size_type i = 0; i < m_len; ++i) {
             copyDataAt(val, i);
         }
         return *this;
     }
 
-    constexpr void reserve(size_type newCap) {
+    void reserve(size_type newCap) {
         if (newCap <= m_cap) {
             if constexpr (dataIsTrivial) {
                 m_len = m_len > newCap ? newCap : m_len;

b4c3f26e01e9434f66bb2d4a406ae8190ca9fef0
MartinNikolovMarinov <martin_n_marinov@protonmail.com>
2023-07-25 12:20:41 +0300

    Architecture detection code update.

--- src/system_checks.h
+++ src/system_checks.h
@@ -42,48 +42,41 @@
     #define OS_UNKNOWN 0
 #endif
 
-// Most of these are unsupported, but the detection code is good.
-// Credit to FreakAnon's answer here: https://stackoverflow.com/a/66249936 .
+// Most of these are unsupported, and will never be supported, but the detection code is good.
 #if defined(__x86_64__) || defined(_M_X64)
-    #define CPU_ARCH "x86_64";
+    // Supported
+    #define CPU_ARCH "x86_64"
+    #define CPU_ARCH_X86_64 1
 #elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
-    #define CPU_ARCH "x86_32";
-#elif defined(__ARM_ARCH_2__)
-    #define CPU_ARCH "ARM2";
-#elif defined(__ARM_ARCH_3__) || defined(__ARM_ARCH_3M__)
-    #define CPU_ARCH "ARM3";
-#elif defined(__ARM_ARCH_4T__) || defined(__TARGET_ARM_4T)
-    #define CPU_ARCH "ARM4T";
-#elif defined(__ARM_ARCH_5_) || defined(__ARM_ARCH_5E_)
-    #define CPU_ARCH "ARM5"
-#elif defined(__ARM_ARCH_6T2_) || defined(__ARM_ARCH_6T2_)
-    #define CPU_ARCH "ARM6T2";
-#elif defined(__ARM_ARCH_6__) || defined(__ARM_ARCH_6J__) || defined(__ARM_ARCH_6K__) || defined(__ARM_ARCH_6Z__) || defined(__ARM_ARCH_6ZK__)
-    #define CPU_ARCH "ARM6";
-#elif defined(__ARM_ARCH_7__) || defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
-    #define CPU_ARCH "ARM7";
-#elif defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
-    #define CPU_ARCH "ARM7A";
-#elif defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
-    #define CPU_ARCH "ARM7R";
-#elif defined(__ARM_ARCH_7M__)
-    #define CPU_ARCH "ARM7M";
-#elif defined(__ARM_ARCH_7S__)
-    #define CPU_ARCH "ARM7S";
+    // TODO: Will be supported.
+    #define CPU_ARCH "x86_32"
+    #define CPU_ARCH_X86_32 1
 #elif defined(__aarch64__) || defined(_M_ARM64)
-    #define CPU_ARCH "ARM64";
+    // TODO: Will be supported.
+    #define CPU_ARCH "ARM64"
+    #define CPU_ARCH_ARM64 1
+#elif defined(__arm__)
+    #define CPU_ARCH "ARM32"
 #elif defined(mips) || defined(__mips__) || defined(__mips)
-    #define CPU_ARCH "MIPS";
+    #define CPU_ARCH "MIPS"
 #elif defined(__sh__)
-    #define CPU_ARCH "SUPERH";
-#elif defined(__powerpc) || defined(__powerpc__) || defined(__powerpc64__) || defined(__POWERPC__) || defined(__ppc__) || defined(__PPC__) || defined(_ARCH_PPC)
-    #define CPU_ARCH "POWERPC";
+    #define CPU_ARCH "SUPERH"
 #elif defined(__PPC64__) || defined(__ppc64__) || defined(_ARCH_PPC64)
-    #define CPU_ARCH "POWERPC64";
+    #define CPU_ARCH "POWERPC64"
+#elif defined(__powerpc) || defined(__powerpc__) || defined(__powerpc64__) || defined(__POWERPC__) || defined(__ppc__) || defined(__PPC__) || defined(_ARCH_PPC)
+    #define CPU_ARCH "POWERPC"
 #elif defined(__sparc__) || defined(__sparc)
-    #define CPU_ARCH "SPARC";
+    #define CPU_ARCH "SPARC"
 #elif defined(__m68k__)
-    #define CPU_ARCH "M68K";
+    #define CPU_ARCH "M68K"
+#elif defined(__riscv) || defined(__riscv__) || defined(RISCVEL)
+    #if __riscv_xlen == 32
+        #define CPU_ARCH "RISC-V32"
+    #elif __riscv_xlen == 64
+        #define CPU_ARCH "RISC-V64"
+    #else
+        #define CPU_ARCH "UNKNOWN RISC-V"
+    #endif
 #else
-    #define CPU_ARCH "UNKNOWN";
+    #define CPU_ARCH "UNKNOWN"
 #endif

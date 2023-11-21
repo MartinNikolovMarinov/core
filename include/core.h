d5b3808a7ad32b577f7c731d5d735c3b4edcdb66
Martin Marinov <martin_n_marinov@protonmail.com>
2023-11-21 13:46:05 +0200

    [REWORK] Clean start.

---
+++ include/core.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "core_API.h"
+#include "core_hash.h"
+#include "core_traits.h"
+#include "core_types.h"
+#include "core_utils.h"
---
+++ test_init_core.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <core.h>
+
+using namespace coretypes;

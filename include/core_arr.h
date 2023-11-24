b14a929e46929d98411b98a54b0d3bba4083ad97
Martin Marinov <martin_n_marinov@protonmail.com>
2023-11-24 20:20:33 +0200

    [REWORK] Arr and expected plus tests added.

---
+++ include/core_arr.h
@@ -0,0 +1,237 @@
+#pragma once
+
+#include <core_types.h>
+#include <core_alloc.h>
+#include <core_mem.h>
+#include <core_traits.h>
+
+namespace core {
+
+using namespace coretypes;
+
+template<typename T, typename TAllocator = CORE_DEFAULT_ALLOCATOR()>
+struct arr {
+    using DataType      = T;
+    using SizeType      = addr_size;
+    using AllocatorType = TAllocator;
+
+    static_assert(core::AllocatorConcept<AllocatorType>, "The allocator template argument must satisfy the AllocatorConcept");
+
+    static constexpr bool dataIsTrivial = core::is_trivial_v<DataType>;
+    static constexpr bool dataHasTrivialDestructor = core::is_trivially_destructible_v<DataType>;
+
+    arr() : m_data(nullptr), m_cap(0), m_len(0) {}
+    arr(SizeType len) : m_data(nullptr), m_cap(len), m_len(len) {
+        if (m_cap > 0) {
+            m_data = reinterpret_cast<DataType *>(AllocatorType::alloc(m_cap * sizeof(DataType)));
+            Assert(m_data != nullptr);
+            callDefaultCtorsIfTypeIsNonTrivial();
+        }
+    }
+    arr(SizeType len, SizeType cap) : m_data(nullptr), m_cap(cap), m_len(len) {
+        Assert(m_cap >= m_len);
+        if (m_cap > 0) {
+            m_data = reinterpret_cast<DataType *>(AllocatorType::alloc(m_cap * sizeof(DataType)));
+            Assert(m_data != nullptr);
+            callDefaultCtorsIfTypeIsNonTrivial();
+        }
+    }
+    arr(const arr<T, AllocatorType>& other) = delete; // prevent copy ctor
+    arr(arr<T, AllocatorType>&& other) : m_data(other.m_data), m_cap(other.m_cap), m_len(other.m_len) {
+        if (this == &other) return;
+        other.m_data = nullptr;
+        other.m_cap = 0;
+        other.m_len = 0;
+    }
+    ~arr() { free(); }
+
+    arr<T, AllocatorType>& operator=(const arr<T, AllocatorType>& other) = delete; // prevent copy assignment
+    arr<T, AllocatorType>& operator=(SizeType) = delete; // prevent size assignment
+    arr<T, AllocatorType>& operator=(arr<T, AllocatorType>&& other) {
+        if (this == &other) return *this;
+        free(); // very important to free before assigning new data.
+        m_data = other.m_data;
+        m_cap = other.m_cap;
+        m_len = other.m_len;
+        other.m_data = nullptr;
+        other.m_cap = 0;
+        other.m_len = 0;
+        return *this;
+    }
+
+    SizeType   cap()     const { return m_cap; }
+    SizeType   byteCap() const { return m_cap * sizeof(DataType); }
+    SizeType   len()     const { return m_len; }
+    SizeType   byteLen() const { return m_len * sizeof(DataType); }
+    DataType*  data()    const { return m_data; }
+    bool       empty()   const { return m_len == 0; }
+
+    void clear() {
+        if constexpr (!dataHasTrivialDestructor) {
+            // For elements that are not trivially destructible call destructors manually:
+            for (SizeType i = 0; i < m_len; ++i) {
+                m_data[i].~T();
+            }
+        }
+        m_len = 0;
+    }
+
+    arr<T, AllocatorType> copy() {
+        DataType* dataCopy = nullptr;
+        if (m_cap > 0) {
+            dataCopy = reinterpret_cast<DataType *>(AllocatorType::alloc(m_cap * sizeof(DataType)));
+            Assert(dataCopy != nullptr);
+            if constexpr (dataIsTrivial) {
+                core::memcopy(dataCopy, m_data, m_len * sizeof(DataType));
+            }
+            else {
+                for (SizeType i = 0; i < m_len; i++) {
+                    auto& rawBytes = dataCopy[i];
+                    new (reinterpret_cast<void*>(&rawBytes)) DataType(m_data[i]);
+                }
+            }
+        }
+        arr<T, AllocatorType> result;
+        result.m_data = dataCopy;
+        result.m_cap = m_cap;
+        result.m_len = m_len;
+        return result;
+    }
+
+    void free() {
+        if (m_data == nullptr) return;
+        clear();
+        m_cap = 0;
+        AllocatorType::free(m_data);
+        m_data = nullptr;
+    }
+
+    DataType& at(SizeType idx)                     { Assert(idx < m_len); return m_data[idx]; }
+    const DataType& at(SizeType idx)         const { Assert(idx < m_len); return m_data[idx]; }
+    DataType& operator[](SizeType idx)             { return at(idx); }
+    const DataType& operator[](SizeType idx) const { return at(idx); }
+
+    DataType& first()             { return at(0); }
+    const DataType& first() const { return at(0); }
+    DataType& last()              { return at(m_len - 1); }
+    const DataType& last()  const { return at(m_len - 1); }
+
+    arr<T, AllocatorType>& append(const DataType& val) {
+        if (m_len == m_cap) {
+            reserve(m_cap == 0 ? 1 : m_cap * 2);
+        }
+        copyDataAt(val, m_len);
+        m_len++;
+        return *this;
+    }
+
+    arr<T, AllocatorType>& append(DataType&& val) {
+        if (m_len == m_cap) {
+            reserve(m_cap == 0 ? 1 : m_cap * 2);
+        }
+        stealDataAt(core::move(val), m_len);
+        m_len++;
+        return *this;
+    }
+
+    arr<T, AllocatorType>& append(const DataType* val, SizeType len) {
+        if (m_len + len > m_cap) {
+            reserve(m_cap <= len ? (m_cap*2 + len) : m_cap * 2);
+        }
+        copyDataAt(val, m_len, len);
+        m_len += len;
+        return *this;
+    }
+
+    arr<T, AllocatorType>& append(const arr<T, AllocatorType>& other) {
+        return append(other.m_data, other.m_len);
+    }
+
+    arr<T, AllocatorType>& fill(const DataType& val) {
+        if constexpr (dataIsTrivial) {
+            core::memfill(m_data, m_len, val);
+        }
+        else {
+            for (SizeType i = 0; i < m_len; ++i) {
+                copyDataAt(val, i);
+            }
+        }
+        return *this;
+    }
+
+    void reserve(SizeType newCap) {
+        if (newCap <= m_cap) {
+            if constexpr (dataIsTrivial) {
+                m_len = m_len > newCap ? newCap : m_len;
+            }
+            else {
+                // For elements that are not trivially destructible call destructors manually:
+                for (SizeType i = newCap; i < m_len; ++i) {
+                    m_data[i].~T();
+                }
+                m_len = m_len > newCap ? newCap : m_len;
+            }
+            m_cap = newCap;
+            return;
+        }
+
+        // reallocate
+        DataType* newData = reinterpret_cast<DataType *>(AllocatorType::alloc(newCap * sizeof(DataType)));
+        // NOTE: Choosing not to clear the new memory here might bite me on the ass later.
+        Assert(newData != nullptr);
+        if (m_data != nullptr) {
+            core::memcopy(newData, m_data, m_len * sizeof(DataType));
+            AllocatorType::free(m_data);
+        }
+        m_data = newData;
+        m_cap = newCap;
+    }
+
+private:
+    DataType *m_data;
+    SizeType m_cap;
+    SizeType m_len;
+
+    inline void stealDataAt(DataType&& rval, SizeType pos) {
+        auto& rawBytes = m_data[pos];
+        new (reinterpret_cast<void*>(&rawBytes)) DataType(core::move(rval));
+    }
+
+    inline void copyDataAt(const DataType& lval, SizeType pos) {
+        if constexpr (dataIsTrivial) {
+            m_data[pos] = lval;
+        }
+        else {
+            auto& rawBytes = m_data[pos];
+            new (reinterpret_cast<void*>(&rawBytes)) DataType(lval);
+        }
+    }
+
+    inline void copyDataAt(const DataType* pval, SizeType pos, SizeType len) {
+        if constexpr (dataIsTrivial) {
+            core::memcopy(m_data + pos, pval, len * sizeof(DataType));
+        }
+        else {
+            for (SizeType i = 0; i < len; i++) {
+                auto& rawBytes = m_data[i + pos];
+                new (reinterpret_cast<void*>(&rawBytes)) DataType(pval[i]);
+            }
+        }
+    }
+
+    inline void callDefaultCtorsIfTypeIsNonTrivial() {
+        // This is exactly the same as calling fill(T()) but it has slightly less overhead.
+        if constexpr (!dataIsTrivial) {
+            for (SizeType i = 0; i < m_len; ++i) {
+                new (&m_data[i]) DataType();
+            }
+        }
+        else {
+            core::memfill(m_data, m_len, 0);
+        }
+    }
+};
+
+static_assert(core::is_standard_layout_v<arr<i32>>, "arr<i32> must be standard layout");
+
+} // namespace core
--- tests/index.h
+++ tests/index.h
@@ -1,10 +1,103 @@
 #pragma once
 
+#define CORE_DEFAULT_ALLOCATOR() StdAllocator
+
 #include <core.h>
 
 #include <iostream>
 
 using namespace coretypes;
+
+inline void coreInit() {
+    core::setGlobalAssertHandler([](const char* failedExpr, const char* file, i32 line, const char* errMsg) {
+        constexpr u32 stackFramesToSkip = 3;
+        constexpr addr_size stackTraceBufferSize = 4096;
+        char trace[stackTraceBufferSize] = {};
+        addr_size traceLen = 0;
+        core::stacktrace(trace, stackTraceBufferSize, traceLen, 200, stackFramesToSkip);
+        std::cout << ANSI_RED_START() << ANSI_BOLD_START()
+                  << "[ASSERTION]:\n  [EXPR]: " << failedExpr
+                  << "\n  [FILE]: " << file << ":" << line
+                  << "\n  [MSG]: " << errMsg
+                  << ANSI_RESET()
+                  << std::endl;
+        std::cout << ANSI_BOLD_START() << "[TRACE]:\n" << trace << ANSI_RESET() << std::endl;
+        throw std::runtime_error("Assertion failed!");
+    });
+}
+
+struct ConstructorTester {
+    static constexpr i32 defaultValue = 7;
+
+    i32 a;
+    ConstructorTester() : a(defaultValue) { g_defaultCtorCalled++; }
+    ConstructorTester(const ConstructorTester& other) : a(other.a) { g_copyCtorCalled++; }
+    ConstructorTester(ConstructorTester&& other) : a(core::move(other.a)) { g_moveCtorCalled++; }
+    ~ConstructorTester() { g_destructorsCalled++; }
+
+    ConstructorTester& operator=(const ConstructorTester& other) {
+        a = other.a;
+        g_assignmentsCopyCalled++;
+        return *this;
+    }
+
+    ConstructorTester& operator=(ConstructorTester&& other) {
+        a = core::move(other.a);
+        g_assignmentsMoveCalled++;
+        return *this;
+    }
+
+    static void resetCtors() {
+        g_defaultCtorCalled = 0;
+        g_copyCtorCalled = 0;
+        g_moveCtorCalled = 0;
+    }
+
+    static void resetDtors() {
+        g_destructorsCalled = 0;
+    }
+
+    static void resetAssignments() {
+        g_assignmentsCopyCalled = 0;
+        g_assignmentsMoveCalled = 0;
+    }
+
+    static void resetAll() {
+        resetCtors();
+        resetDtors();
+        resetAssignments();
+    }
+
+    static i32 defaultCtorCalled()      { return g_defaultCtorCalled; }
+    static i32 copyCtorCalled()         { return g_copyCtorCalled; }
+    static i32 moveCtorCalled()         { return g_moveCtorCalled; }
+    static i32 totalCtorsCalled()       { return g_defaultCtorCalled + g_copyCtorCalled + g_moveCtorCalled; }
+    static i32 assignmentsCopyCalled()  { return g_assignmentsCopyCalled; }
+    static i32 assignmentsMoveCalled()  { return g_assignmentsMoveCalled; }
+    static i32 assignmentsTotalCalled() { return g_assignmentsCopyCalled + g_assignmentsMoveCalled; }
+    static i32 dtorsCalled()            { return g_destructorsCalled; }
+    static bool noCtorsCalled()         { return totalCtorsCalled() == 0; }
+
+private:
+    inline static i32 g_destructorsCalled;
+    inline static i32 g_defaultCtorCalled;
+    inline static i32 g_copyCtorCalled;
+    inline static i32 g_moveCtorCalled;
+    inline static i32 g_assignmentsCopyCalled;
+    inline static i32 g_assignmentsMoveCalled;
+};
+
+using CT = ConstructorTester;
+
+struct StaticVariableDefaultCtorTester {
+    // NOTE: This is a terrible idea, but it still should work.
+    inline static i32 nextId = 0;
+    i32 a = nextId++;
+};
+
+using SVCT = StaticVariableDefaultCtorTester;
+
+// ##################### Helper functions for testing ##################################################################
 
 /**
  * \brief This code is quite complex, because it does zero allocations, but it's purpose is quite simple. It iterates over
@@ -31,24 +124,6 @@ constexpr void executeTestTable(const char (&errMsgPrefix)[PLen], const TCase (&
     }
 }
 
-inline void coreInit() {
-    core::setGlobalAssertHandler([](const char* failedExpr, const char* file, i32 line, const char* errMsg) {
-        constexpr u32 stackFramesToSkip = 3;
-        constexpr addr_size stackTraceBufferSize = 4096;
-        char trace[stackTraceBufferSize] = {};
-        addr_size traceLen = 0;
-        core::stacktrace(trace, stackTraceBufferSize, traceLen, 200, stackFramesToSkip);
-        std::cout << ANSI_RED_START() << ANSI_BOLD_START()
-                  << "[ASSERTION]:\n  [EXPR]: " << failedExpr
-                  << "\n  [FILE]: " << file << ":" << line
-                  << "\n  [MSG]: " << errMsg
-                  << ANSI_RESET()
-                  << std::endl;
-        std::cout << ANSI_BOLD_START() << "[TRACE]:\n" << trace << ANSI_RESET() << std::endl;
-        throw std::runtime_error("Assertion failed!");
-    });
-}
-
 inline i32 g_testCount = 0;
 
 #define RunTest(test, ...)                                                                      \
@@ -70,16 +145,21 @@ inline i32 g_testCount = 0;
     suite(__VA_ARGS__);                                                \
     std::cout << "[SUITE " << ANSI_BOLD(ANSI_GREEN("PASSED")) << "] " << ANSI_BOLD(#suite) << std::endl;
 
-i32 runDeferTestsSuite();
+// ##################### Test suites ###################################################################################
+
+i32 runArrTestsSuite();
+i32 runBitsTestsSuite();
 i32 runCptrConvTestsSuite();
 i32 runCptrTestsSuite();
-i32 runMemTestsSuite();
+i32 runDeferTestsSuite();
+i32 runExpectedTestsSuite();
 i32 runHashTestsSuite();
-i32 runTupleTestsSuite();
-i32 runIntsTestsSuite();
 i32 runIntrinsicsTestsSuite();
+i32 runIntsTestsSuite();
 i32 runMathTestsSuite();
+i32 runMemTestsSuite();
 i32 runTraitsTestsSuite();
+i32 runTupleTestsSuite();
 
 i32 runBumpAllocatorTestsSuite();
 i32 runStdAllocatorTestsSuite();
@@ -92,16 +172,19 @@ inline i32 runAllTests() {
 
     std::cout << "\n" << "RUNNING TESTS" << "\n\n";
 
-    RunTestSuite(runDeferTestsSuite);
+    RunTestSuite(runArrTestsSuite);
+    RunTestSuite(runBitsTestsSuite);
     RunTestSuite(runCptrConvTestsSuite);
     RunTestSuite(runCptrTestsSuite);
-    RunTestSuite(runMemTestsSuite);
+    RunTestSuite(runDeferTestsSuite);
+    RunTestSuite(runExpectedTestsSuite);
     RunTestSuite(runHashTestsSuite);
-    RunTestSuite(runTupleTestsSuite);
-    RunTestSuite(runIntsTestsSuite);
     RunTestSuite(runIntrinsicsTestsSuite);
+    RunTestSuite(runIntsTestsSuite);
     RunTestSuite(runMathTestsSuite);
+    RunTestSuite(runMemTestsSuite);
     RunTestSuite(runTraitsTestsSuite);
+    RunTestSuite(runTupleTestsSuite);
 
     RunTestSuite(runBumpAllocatorTestsSuite);
     RunTestSuite(runStdAllocatorTestsSuite);

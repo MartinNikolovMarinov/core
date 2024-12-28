0c7bd1f4bea57fc758d5bce532737e7334cd7be9
Martin Marinov <martin_n_marinov@protonmail.com>
2024-12-28 14:39:58 +0200

    new allocator concept sandboxed

--- core_sandbox.cpp
+++ core_sandbox.cpp
@@ -28,8 +28,80 @@ void assertHandler(const char* failedExpr, const char* file, i32 line, const cha
     throw std::runtime_error("Assertion failed!");
 };
 
+using AllocateFn             = void *(*)(addr_size count, addr_size size);
+using ZeroAllocateFn         = void *(*)(addr_size count, addr_size size);
+using FreeFn                 = void (*)(void *ptr, addr_size count, addr_size size);
+using ClearFn                = void (*)();
+using TotalMemoryAllocatedFn = addr_size (*)();
+using InUseMemoryFn          = addr_size (*)();
+
+struct AllocatorContext {
+    AllocateFn alloc;
+    ZeroAllocateFn calloc;
+    FreeFn free;
+    ClearFn clear;
+    TotalMemoryAllocatedFn totalMemoryAllocated;
+    InUseMemoryFn inUseMemory;
+};
+
+AllocatorContext g_registeredAllocators[5];
+
+template <i32 TAllocId>
+inline AllocatorContext& selectAllocator() {
+    return g_registeredAllocators[TAllocId];
+}
+
+template <i32 TAllocId = 0>
+struct Tester {
+    inline static AllocatorContext& allocator = selectAllocator<TAllocId>();
+
+    void* oneByte() {
+        return allocator.alloc(1, 1);
+    }
+
+    void freeByte(void* addr) {
+        allocator.free(addr, 1, 1);
+    }
+
+    i32 someMember;
+};
+
+static_assert(sizeof(Tester<0>) == sizeof(i32), "The allocator must not take any space!");
+
 i32 main() {
     core::initProgramCtx(assertHandler, nullptr);
+
+    g_registeredAllocators[0].alloc = [](addr_size count, addr_size size) {
+        void* ret = std::malloc(count * size);
+        std::cout << "Called Malloc 0" << std::endl;
+        return ret;
+    };
+    g_registeredAllocators[0].free = [](void* ptr, addr_size, addr_size) {
+        std::free(ptr);
+        std::cout << "Called Free 0" << std::endl;
+    };
+
+    g_registeredAllocators[1].alloc = [](addr_size count, addr_size size) {
+        void* ret = std::malloc(count * size);
+        std::cout << "Called Malloc 1" << std::endl;
+        return ret;
+    };
+    g_registeredAllocators[1].free = [](void* ptr, addr_size, addr_size) {
+        std::free(ptr);
+        std::cout << "Called Free 1" << std::endl;
+    };
+
+    {
+        Tester tester;
+        char* byte = reinterpret_cast<char*>(tester.oneByte());
+        tester.freeByte(byte);
+    }
+
+    {
+        Tester<1> tester;
+        char* byte = reinterpret_cast<char*>(tester.oneByte());
+        tester.freeByte(byte);
+    }
 
     return 0;
 }
